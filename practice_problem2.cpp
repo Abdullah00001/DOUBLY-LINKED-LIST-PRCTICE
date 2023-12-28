c528897b7238efeb5dfa28fb583b714c9e894e73
ABDULLAH <abdullahbinomarchowdhury02@gmail.com>
2023-12-28 22:03:47 +0600

    solve the problem 2

---
+++ practice_problem2.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *prev;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+void addNodes(Node *&head, Node *&tail, int value)
+{
+    Node *newNode = new Node(value);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+void rvrsNodes(Node *head, Node *tail)
+{
+    Node *i = head, *j = tail;
+    while (i != j && i->next != j)
+    {
+        swap(i->value, j->value);
+        i = i->next;
+        j = j->prev;
+    }
+    swap(i->value, j->value);
+}
+void prntNode(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->value << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = NULL, *tail = NULL;
+    int value;
+    while (true)
+    {
+        cin >> value;
+        if (value == -1)
+        {
+            break;
+        }
+        addNodes(head, tail, value);
+    }
+    rvrsNodes(head, tail);
+    prntNode(head);
+    return 0;
+}
